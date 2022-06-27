044d9830cf2b7b4f619b2ee0c8e16927880b9165
lavo7873 <linhuang1101@gmail.com>
2022-06-26 21:45:05 -0800

    update

---
+++ Exercise/exercise_04.py
@@ -0,0 +1,28 @@
+"""
+Created on Sun Jun 26 20:14:46 2022
+
+@author: linhuang
+
+exercise 04
+"""
+print ("Hello Worlds")
+# next
+print ("Hello Worlds");print(5)
+#next
+print("Xin","Chao","Lam Vong", 1234 , sep=":")
+#next
+print('Xin','Chào',end=' : ') 
+#end se la diem ket thuc va khong xuong hang
+print('TITV')
+#next
+print('name={0},surname={1}'.format('titv', 'vn'))
+
+"""
+print(object(s), sep=seprator, end=end, file=file, flush=flush)
+.object(s): đối tượng, dữ liệu được xuất
+.sep=seprator: ngăn cách
+.end=end : kết thúc
+.file=file: tệp tập tin
+flush=flush: đẩy dữ liệu
+
+"""
---
+++ Exercise/exercise_05.py
@@ -0,0 +1,13 @@
+"""
+Create 06/26/2022 10:25PM
+@author: Linhuang
+"""
+#input nghĩa là nhập vào số bất kì để mình có thể gõ vào nó
+input("Nhập số bất kì:")
+a = input("nhập vào a:")
+print("a = {0} ".format(a))
+
+ho = input("Nhập vào họ :");
+ten = input("Nhập vào tên :");
+
+print("Xin Chào", ho , ten)
