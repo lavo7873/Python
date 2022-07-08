db51e3aea5b0131a77a46b3ad39bbc2790006de4
lavo7873 <linhuang1101@gmail.com>
2022-07-08 06:31:33 -0800

    update

---
+++ turtle/star_04.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle
+
+t.speed(300)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.right(140)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(160)
+t.forward(150)
+t.left(70)
+t.forward(130)
+t.right(150)
+t.forward(110)
+t.left(85)
+t.forward(120)
+
+turtle.done()
