886d694d5adaf7bf1a83123edf04daba96e83c4d
lavo7873 <linhuang1101@gmail.com>
2022-07-09 16:09:52 -0800

    update

--- extra/Ex_04.py
+++ extra/Ex_04.py
@@ -6,10 +6,50 @@ pensize(2)
 
 for i in range (5):
     left(82)
-    for i in range (4):
-        forward(50)
-        left(90)
-        forward(100)
+    from turtle import *
 
-    
+from random import randint
+speed(2)
+pensize(2)
+
+
+#1-box
+for i in range (5):
+    left(82)
+for i in range(4):
+    forward(50)
+    left(90)
+    forward(50)
+
+#2-box
+for i in range (5):
+    left(87)
+for i in range(4):
+    forward(50)
+    left(90)
+    forward(50)
+
+#3-box
+for i in range (5):
+    left(87)
+for i in range(4):
+    forward(50)
+    left(90)
+    forward(50)
+
+#4-box
+for i in range (5):
+    left(87)
+for i in range(4):
+    forward(50)
+    left(90)
+    forward(50)
+
+#5-box
+for i in range (5):
+    left(86)
+for i in range(4):
+    forward(50)
+    left(90)
+    forward(50)
 done()
