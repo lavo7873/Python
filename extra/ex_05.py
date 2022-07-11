f16a36b23005fe4a342e5694e378a8906a50c8fa
lavo7873 <linhuang1101@gmail.com>
2022-07-11 05:37:33 -0800

    update

--- extra/ex_05.py
+++ extra/ex_05.py
@@ -1,6 +1,8 @@
 from turtle import *
 from random import randint
 
+canh1 = 171
+canh2 = 121
 
 speed(15)
 pensize(2)
@@ -10,21 +12,21 @@ for i in range (8):
     forward(50)
     left(90)
     forward(50)
-forward(121)
+forward(canh2)
 left(90)
-forward(171)
+forward(canh1)
 left(90)
-forward(171)
+forward(canh1)
 left(90)
-forward(121)
+forward(canh2)
 left(45)
-forward(121)
+forward(canh2)
 left(90)
-forward(171)
+forward(canh1)
 left(90)
-forward(171)
+forward(canh1)
 left(90)
-forward(171)
+forward(canh1)
 
   
 done()
--- extra/try.py
+++ extra/try.py
@@ -1,20 +1,3 @@
-import turtle
-t = turtle.Turtle()
-
-t.pendown()
-
-t.penup()
-
-t.forward(100)
-
-t.right(90)
-
-t.forward(200)
-
-t.right(90)
-
-t.forward(100)
-
-t.right(90)
-
-t.forward(200)
+status = "Incomplete"
+status = "Complete"
+print(status)
