ca17b727e23453a654386000cd73433225a3f4f3
lavo7873 <linhuang1101@gmail.com>
2022-07-15 15:40:27 -0800

    update

---
+++ Exercise/exercise_06.py
@@ -0,0 +1,12 @@
+ten1 = input("Tên học viên 1: ")
+tuoi1 = int (input("Tuổi học viên 1:"))
+
+ten2 = input("Tên học viên 2: ")
+tuoi2 = int (input("Tuổi học viên 2:"))
+
+ten3 = input("Tên học viên 3: ")
+tuoi3 = int (input("Tuổi học viên 3:"))
+
+aver = (tuoi1 + tuoi2 + tuoi3)/3
+
+print(f"Tên học viên: {ten1}. Aver: {aver:.4f}")
