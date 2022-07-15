36614dc36f10182195e5b3f714bfb37cc58b51db
lavo7873 <linhuang1101@gmail.com>
2022-07-14 22:09:46 -0800

    update

--- extra/ex_07.py
+++ extra/ex_07.py
@@ -1,14 +1,27 @@
+#importing turtle
 import turtle
+#function to draw square
+def draw_square(t, size):
+    #color for each side
+    for c in ['red', 'green', 'blue','gray']:
+        t.color(c)
+        t.forward(size)
+        t.left(90)
+#setup the window       
+turtle.setup(600,1000)
 
-colors = ['blue','red','green',]
-
-t = turtle.Pen()
-
-
-for x in range(360):
-   t.pencolor(colors[x%2])
-   t.width(x//100 + 1)
-   t.forward(x)
-   t.left(90)
-
-turtle.done()
+w = turtle.Screen()
+#turtle object
+te = turtle.Turtle()
+#pensize
+te.pensize(2)
+#first sqaure size
+p_size = 15
+for i in range(25):
+    #function call
+    draw_square(te, p_size)
+    #increasing size
+    p_size = p_size +18      
+    te.forward(16)        
+    te.right(16)      
+w.exitonclick()
