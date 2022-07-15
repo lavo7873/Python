846516808e656f97a7b84403d18a46d4a302a367
lavo7873 <linhuang1101@gmail.com>
2022-07-14 18:48:49 -0800

    update

--- CodeG/changedegree.py
+++ CodeG/changedegree.py
@@ -2,6 +2,6 @@
 Công Thức Chuyển Đổi:
 C/5 = (F-32)/9
 """
-C = 5
-F = (9*C)/5 + 32
-print (F)
+c = input(" Enter temperature in Centigrade: ")
+f = (9*(int(c))/5)+32
+print(" Temperature in Fahrenheit is: ", f)
