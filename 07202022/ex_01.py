3264e0447f658e05a6ddc367b640fa56e2eab1c5
lavo7873 <linhuang1101@gmail.com>
2022-07-20 06:22:28 -0800

    update

--- 07202022/ex_01.py
+++ 07202022/ex_01.py
@@ -1,5 +1,5 @@
 n = int(input('Nhập vào một số nguyên :'))
-if (n < 99999999 and n >0):
-    print ("True")
-else (n < 0):
-    print("False")
+if n > 0:
+    print ("Giá trị tuyệt đối {n} :")
+else n < 0:
+    print("Giá trị tuyệt đối {-n} :")
