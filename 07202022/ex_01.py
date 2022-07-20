915819c2c4dba38385c9e1c5c359b8b95f8a4f92
lavo7873 <linhuang1101@gmail.com>
2022-07-20 15:57:26 -0800

    update

--- 07202022/ex_01.py
+++ 07202022/ex_01.py
@@ -1,5 +1,5 @@
 n = int(input('Nhập vào một số nguyên :'))
-if n > 0:
-    print ("Giá trị tuyệt đối {n} :")
-else n < 0:
-    print("Giá trị tuyệt đối {-n} :")
+if n > 0 and n < 10:
+    print (f"Giá trị tuyệt đối {n} :")
+else :
+    print(f"Giá trị tuyệt đối {n} : {(-1*n)}")
