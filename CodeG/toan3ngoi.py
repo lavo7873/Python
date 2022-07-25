62e81339ffb8c8481f31d46d3c5be2f818875059
lavo7873 <linhuang1101@gmail.com>
2022-07-25 04:41:52 -0800

    update

---
+++ CodeG/toan3ngoi.py
@@ -0,0 +1,15 @@
+num_check = 2
+
+# USING TERNARY OPERATOR
+msg = "Even" if num_check %2 == 0 else "Odd"
+print(msg)
+
+# USING USUAL IF-ELSE
+msg = ""
+if(num_check%2  == 0):
+    msg = "Even"
+else:
+    msg = "Odd"
+print(msg)
+
+#Another
---
+++ Exercise/exercise_07.py
@@ -0,0 +1,12 @@
+ten1 = input("Nhập tên học viên 1 :")
+tuoi1 = int(input("Nhập tuổi học viên 1 :"))
+
+ten2 = input("Nhập tên học viên 2 :")
+tuoi2 = int(input("Nhập tuổi học viên 2 :"))
+
+ten3 = input("Nhập tên học viên 3 :")
+tuoi3 = int(input("Nhập tuổi học viên 3 :"))
+
+ttb = (tuoi1 + tuoi2 + tuoi3) / 3
+
+print(f"Tuổi Trung Bình Của học viên {tuoi1} , {tuoi2} , {tuoi3} là : {ttb:2f}")
