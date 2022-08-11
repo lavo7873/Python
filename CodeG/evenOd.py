a07364af4e59ff1da4a0238dc5ad8a18c9e36421
lavo7873 <linhuang1101@gmail.com>
2022-08-11 15:17:17 -0800

    update

---
+++ CodeG/evenOd.py
@@ -0,0 +1,23 @@
+def sap_xep(num1, num2, num3):
+    if num2 < num1 and num2 < num3:
+        temp = num1
+        num1 = num2
+        num2 = temp
+
+    elif num3 < num1 and num3 < num2:
+        temp = num1
+        num1 = num3
+        num3 = temp
+
+    if num3 < num2:
+        temp = num2
+        num2 = num3
+        num3 = temp
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+a, b, c = sort_numbers(x, y, z)
+print("Original numbers: ", x, y, z)
+print("Sorted numbers: ", a, b, c)
