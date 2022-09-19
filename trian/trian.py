0f0c3e9a69c416ab430e1b44f77d94878e162068
lavo7873 <linhuang1101@gmail.com>
2022-09-19 06:36:29 -0800

    update

--- trian/trian.py
+++ trian/trian.py
@@ -1,6 +1,7 @@
-class Triangle(Shape):
-    def __init__(self, side1, side2, side3):
-        super().__init__(3)
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
+for l in 'Jhon':
+
+   if l == 'o':
+
+      pass
+
+   print(l, end=", ")
